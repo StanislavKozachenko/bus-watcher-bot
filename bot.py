b11bbe00582dba0d502a6f10bdfb45bdc62e24ab
StanislavKozachenko <stanislav.03@list.ru>
2025-12-02 22:26:05 +0300

    [fix] fix server_runner conf

--- bot.py
+++ bot.py
@@ -152,4 +152,9 @@ if __name__ == "__main__":
         asyncio.run(runner_local())
     else:
         print("Running in SERVER MODE")
-        asyncio.run(runner_server())
+        # Если loop уже есть, просто добавляем задачу
+        try:
+            loop = asyncio.get_running_loop()
+            loop.create_task(runner_server())
+        except RuntimeError:  # loop нет — создаем
+            asyncio.run(runner_server())
